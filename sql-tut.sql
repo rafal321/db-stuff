64b47e64672396dd992e533e637c0eb5769e530b
Rafal <therafk@hotmail.com>
2020-04-24 23:34:20 +0100

    Update sql-tut.sql

--- sql-tut.sql
+++ sql-tut.sql
@@ -17,30 +17,30 @@ delimiter //
 CREATE PROCEDURE withdraw(IN account_id INT, IN amount NUMERIC(8.2), OUT success BOOL)
 BEGIN
 
-DECLARE current_balance NUMERIC(8,2) DEFAULT 0.0;
+  DECLARE current_balance NUMERIC(8,2) DEFAULT 0.0;
 
-DECLARE exit handler for sqlexception
-BEGIN
-SHOW ERRORS;
-END;
-DECLARE exit handler FOR sqlwarning
-BEGIN
-SHOW WARNINGS;
-END;
+    DECLARE exit handler for sqlexception
+    BEGIN
+    SHOW ERRORS;
+    END;
+    DECLARE exit handler FOR sqlwarning
+    BEGIN
+    SHOW WARNINGS;
+    END;
 
-START TRANSACTION;
+  START TRANSACTION;
 
--- we should also add index to balance column otherwise we lock entire table
-SELECT balance INTO current_balance FROM company1.bank WHERE acc_id=account_id FOR UPDATE;
+    -- we should also add index to balance column otherwise we lock entire table
+    SELECT balance INTO current_balance FROM company1.bank WHERE acc_id=account_id FOR UPDATE;
 
-if current_balance >= amount then
-UPDATE bank SET balance = balance - amount WHERE acc_id = account_id;
-SET success=TRUE;
-else
-SET success=FALSE;
-END if;
+    if current_balance >= amount then
+      UPDATE bank SET balance = balance - amount WHERE acc_id = account_id;
+      SET success=TRUE;
+      else
+      SET success=FALSE;
+    END if;
 
-COMMIT;
+  COMMIT;
 END//
 delimiter ;
 
