9fc971e732a2c0009c19244dc47d5595f3234f4b
Rafal <therafk@hotmail.com>
2020-12-29 22:50:35 +0000

    Update mysql-dump.sh

--- mysql-dump.sh
+++ mysql-dump.sh
@@ -1,9 +1,11 @@
 # to dump and restore with mysqldump only - the Tarik & Emerson way
 
+# databases=db_name
 mysqldump --single-transaction --no-data --skip-triggers -v --databases sakila company > 1-structure-$(date +%F).sql
 mysqldump --single-transaction --no-data --no-create-info --skip-triggers --routines --skip-opt -v --databases sakila company > 2-routines-$(date +%F).sql
 mysqldump --single-transaction --no-data --no-create-info --skip-routines --triggers --skip-opt -v --databases sakila company > 4-triggers-$(date +%F).sql
 mysqldump --single-transaction -n -R -c -t -e -v -K --skip-routines --skip-triggers --databases sakila company > 3-data-$(date +%F).sql
+# mydumper --database=$databases --trx-consistency-only -t 4 -m --rows=500000 --compress -o ./3-data_$(date +%F)
 
 mysql --force < 1-structure-2020-06-20.sql
 mysql --force < 2-routines-2020-06-20.sql
