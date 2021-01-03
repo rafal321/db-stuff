60e0c8153c75c04c3cef8f78f065379468665c33
Rafal <therafk@hotmail.com>
2021-01-03 13:18:39 +0000

    Update sql-handyStatements.sql

--- sql-handyStatements.sql
+++ sql-handyStatements.sql
@@ -3,7 +3,9 @@
 
 /* ________________________________________________________________________________
 https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_BestPractices.html
-The amount of memory required by MySQL for reads and writes depends on the tables involved in the operations. It is a best practice to have at least enough RAM to the hold the indexes of actively used tables. To find the ten largest tables and indexes in a database, use the following query: */
+The amount of memory required by MySQL for reads and writes depends on the tables involved in the operations.
+It is a best practice to have at least enough RAM to the hold the indexes of actively used tables.
+To find the ten largest tables and indexes in a database, use the following query: */
 
 SELECT CONCAT(table_schema, '.', table_name),
        CONCAT(ROUND(table_rows / 1000000, 2), 'M')                                    rows,
